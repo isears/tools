e5269c97e369a9561485e5ed0157aa127e512d2d
Isaac Sears <isaac.j.sears@gmail.com>
2018-02-17 09:25:46 -0500

    Automatically enable ssh access in setup script

--- setup.sh
+++ setup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Setup vim
 cp ./vim/.vimrc ~/.vimrc
 mkdir -p ~/.vim/colors
 cp ./vim/colors/* ~/.vim/colors/
+
+# Enable ssh access
+mkdir -p ~/.ssh
+touch ~/.ssh/authorized_keys
+cat ./pubkeys/id_rsa.pub >> ~/.ssh/authorized_keys
