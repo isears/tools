e4bd86849ac46e71227b0065e21bc7e0eec59222
Isaac Sears <isaac.j.sears@gmail.com>
2018-02-24 14:02:34 -0500

    Add environment variable for 256 colors in tmux

--- setup.sh
+++ setup.sh
@@ -5,7 +5,12 @@ cp ./vim/.vimrc ~/.vimrc
 mkdir -p ~/.vim/colors
 cp ./vim/colors/* ~/.vim/colors/
 
+# vim color support in tmux
+echo 'export TERM="xterm-256color"' >> ~/.bashrc
+
+
 # Enable ssh access
 mkdir -p ~/.ssh
 touch ~/.ssh/authorized_keys
 cat ./pubkeys/id_rsa.pub >> ~/.ssh/authorized_keys
+
