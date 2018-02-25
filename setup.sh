a2e217dfc850f42d3b8cb0ae618494fcc8b4df1b
Isaac Sears <isaac.j.sears@gmail.com>
2018-02-24 21:42:13 -0500

    Add neobundle and airline vim plugins

--- setup.sh
+++ setup.sh
@@ -5,9 +5,13 @@ cp ./vim/.vimrc ~/.vimrc
 mkdir -p ~/.vim/colors
 cp ./vim/colors/* ~/.vim/colors/
 
+# Install neobundle vim plugin manager
+curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh > install.sh
+sh ./install.sh
+rm install.sh
+
 # vim color support in tmux
 echo 'export TERM="xterm-256color"' >> ~/.bashrc
-
 
 # Enable ssh access
 mkdir -p ~/.ssh
